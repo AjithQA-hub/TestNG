14f32e84413c7e565afd063c5a0e991cd84845b5
AJITH <113958920+AjithQA-hub@users.noreply.github.com>
2026-01-12 15:36:54 +0530

    Update TestNGProject6.java

--- src/main/java/org/test/TestNGProject6.java
+++ src/main/java/org/test/TestNGProject6.java
@@ -5,41 +5,42 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.base.BaseClass;
+import org.base.FbLogin;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-public class SampleTestTNG10 {
-	
+public class TestNGProject6 extends BaseClass { // SampleTestTNG10
+ 	
 	@Test
-	private void tc3()
-	{
-		System.out.println("Tc3 : "+Thread.currentThread().getId());
+	private void tc3() {
+		System.out.println("Tc3 : " + Thread.currentThread().getId());
 		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-	}
-	
-	@Test
-	private void tc2()
-	{
-		System.out.println("Tc2 : "+Thread.currentThread().getId());
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://en-gb.facebook.com/");
-		driver.manage().window().maximize();
-		
-		WebElement email = driver.findElement(By.id("email"));
-		email.sendKeys("ajithkumarvam1@gmail.com");
-	}
-	@Test
-	private void tc1()
-	{
-		System.out.println("Tc1 : "+Thread.currentThread().getId());
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new ChromeDriver();
-		driver.get("https://www.flipkart.com/");
-		driver.manage().window().maximize();
-	}
+		driver.close();
 	}
 
+	@Test
+	private void tc2() {
+		System.out.println("Tc2 : " + Thread.currentThread().getId());
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://en-gb.facebook.com/");
+		driver.manage().window().maximize();
+		WebElement email = driver.findElement(By.id("email"));
+		email.sendKeys("ajithkumarvam1@gmail.com");
+		driver.close();
+	}
+
+	@Test
+	private void tc1() {
+		System.out.println("Tc1 : " + Thread.currentThread().getId());
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.redbus.com/");
+		driver.manage().window().maximize();
+		driver.close();
+	}
+}
