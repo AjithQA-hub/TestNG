ec83ff27611d6ec44d46e4965c871e1c3de5654f
AJITH <113958920+AjithQA-hub@users.noreply.github.com>
2026-01-12 15:20:43 +0530

    Update TestNGProject4.java

--- src/main/java/org/test/TestNGProject4.java
+++ src/main/java/org/test/TestNGProject4.java
@@ -1,5 +1,6 @@
 package org.test;
 
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -7,6 +8,8 @@ import org.testng.annotations.Test;
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.event.KeyEvent;
+import java.security.PrivateKey;
+
 import org.base.BaseClass;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -20,6 +23,10 @@ public class TestNGProject4 extends BaseClass {
 		windowMaximize();  
 	}
 	
+	@AfterClass
+	private void closeTheBrowser() {
+		closeEntireBrowser();
+	}
 	@DataProvider(name="Amazon Product")
 	public Object datas()
 	{
@@ -32,10 +39,11 @@ public class TestNGProject4 extends BaseClass {
 				};
 	}
 	@Test(dataProvider = "Amazon Product")
-	private void tc1(String product) throws AWTException
+	private void tc1(String product) throws AWTException, InterruptedException
 	{
 		launchUrl("https://www.amazon.com/");
 		windowMaximize();
+		Thread.sleep(1000);
 		WebElement searchBox = driver.findElement(By.id("twotabsearchtextbox"));
 		searchBox.sendKeys(product);
 		
