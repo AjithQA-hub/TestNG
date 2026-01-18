170e3c105e5e4784d97fa045e570d0cec46dd28a
AJITH <113958920+AjithQA-hub@users.noreply.github.com>
2026-01-18 11:01:53 +0530

    Update BaseClass.java

--- src/main/java/org/base/BaseClass.java
+++ src/main/java/org/base/BaseClass.java
@@ -4,8 +4,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-//import java.nio.file.FileAlreadyExistsException;
-//import java.sql.Date;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -182,32 +180,6 @@ public class BaseClass {
 		FileOutputStream fos=new FileOutputStream(f);
 		wb.write(fos);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	
 
 }
