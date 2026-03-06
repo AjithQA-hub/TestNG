f8791e9f948c177498f21f412b2281f949eb8c58
AJITH V <113958920+AjithQA-hub@users.noreply.github.com>
2026-03-06 12:32:01 +0530

    Update Example.java

--- src/main/java/org/example/Example.java
+++ src/main/java/org/example/Example.java
@@ -1,1 +1,69 @@
+package org.example;
 
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Example {
+
+	@BeforeSuite
+	public void beforeSuite() {
+		System.out.println("Before Suite");
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		System.out.println("Before Test");
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+		System.out.println("Before Class");
+	}
+
+	@BeforeMethod
+	public void beforeMethod() {
+		System.out.println("Before Method");
+	}
+
+	@Test (enabled = false)
+	public void test1() {
+		System.out.println("Test Case 1");
+	}
+
+	@Test (priority = 2)
+	public void test2() {
+		System.out.println("Test Case 2");
+	}
+
+	@Test (priority = 1)
+	public void test3() {
+		System.out.println("Test Case 3");
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+		System.out.println("After Method");
+	}
+
+	@AfterClass
+	public void afterClass() {
+		System.out.println("After Class");
+	}
+
+	@AfterTest
+	public void afterTest() {
+		System.out.println("After Test");
+	}
+
+	@AfterSuite
+	public void afterSuite() {
+		System.out.println("After Suite");
+	}
+}
